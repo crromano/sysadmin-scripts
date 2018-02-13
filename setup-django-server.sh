10b2d534381b0d4a5c0327815a49e04b4c7aa9ae
Iron Fist <32536401+crromano@users.noreply.github.com>
2018-02-13 13:39:50 +0100

    Update setup-django-server.sh

--- setup-django-server.sh
+++ setup-django-server.sh
@@ -8,4 +8,8 @@ sudo -E apt-get -q -y install mysql-server
 sudo apt install -y python3-pip
 sudo apt-get install libapache2-mod-wsgi-py3
 pip3 install virtualenv
-
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-apache 
